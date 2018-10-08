cc575419b55555d325315e212cf7c097d2e79c27
Tesfa <43484797+CallMeTes@users.noreply.github.com>
2018-10-08 01:01:11 -0400

    Copy of my NestedForLoop
    This is a copy of my classwork on 10/3/18

---
+++ NestedForLoop.java
@@ -0,0 +1,16 @@
+//Tesfa Greaves - CMP167//FA2018
+
+public class NestedForLoop
+{
+	public static void main(String[] args)
+	{
+		for(int i=0; i<10; i++)
+		{
+			for(int j=0; j<9; j++)
+			{
+				System.out.print(1);
+			}
+		System.out.println("");
+		}
+	}
+}
